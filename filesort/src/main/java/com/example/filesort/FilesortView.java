20f46ed397934af052972b7926750de0166be838
viv-codes <vhafener@outlook.com>
2022-05-01 22:31:28 -0400

    End of work session - more done on view

--- filesort/src/main/java/com/example/filesort/FilesortView.java
+++ filesort/src/main/java/com/example/filesort/FilesortView.java
@@ -22,8 +22,14 @@ public class FilesortView extends Application {
     public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(FilesortView.class.getResource("filesort-view.fxml"));
 //        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        /**
+         * This vbox is the basic structure of the entire view. All other components reside within this vbox.
+         */
         VBox vBox = new VBox();
 
+        /**
+         * This component is an Hbox used to hold any informational text and stuff at the top of the program, to fill space and serve as a reference
+         */
         HBox textBlurb = new HBox();
         Label label = new Label("Welcome to filesort-gui!\nEventually there will be helpful stuff here.");
         label.setFont(new Font(20));
@@ -31,8 +37,15 @@ public class FilesortView extends Application {
         label.setWrapText(true);
         textBlurb.getChildren().add(label);
 
+        /**
+         * This component handles the buttons used for input and output directory selection
+         */
+        HBox dirChooseBox = new HBox();
+
         DirectoryChooser inputDirChooser = new DirectoryChooser();
-        DirectoryChooser outputDirChooser = new DirectoryChooser(); // TODO This might be redundant, IDK
+        inputDirChooser.setTitle("Filesort - "+VERSION+" - Select input directory");
+        DirectoryChooser outputDirChooser = new DirectoryChooser();
+        outputDirChooser.setTitle("Filesort - "+VERSION+" - Select output directory");
 
         Button inputButton = new Button("Select Input Directory");
         inputButton.setFont(new Font(15));
@@ -43,18 +56,25 @@ public class FilesortView extends Application {
 
         Button outputButton = new Button("Select Output Directory");
         outputButton.setFont(new Font(15));
-        inputButton.setOnAction(e -> {
+        outputButton.setOnAction(e -> {
             File selectedDirectory = outputDirChooser.showDialog(stage);
 //            TODO Here is the hook for model outputdir
         });
 
-        vBox.getChildren().addAll(textBlurb, inputButton, outputButton);
+        dirChooseBox.getChildren().addAll(inputButton, outputButton);
+        dirChooseBox.setMargin(inputButton, new Insets(20,20,20,20));
+        dirChooseBox.setMargin(outputButton, new Insets(20,20,20,20));
+
+//        Adds all necessary components to the vbox and finishes configuration
+        vBox.getChildren().addAll(textBlurb, dirChooseBox);
+
         vBox.setMargin(textBlurb, new Insets(30,30,30,30));
-        vBox.setMargin(inputButton, new Insets(10,30,30,30));
-        vBox.setMargin(outputButton, new Insets(0,5,20,0));
 
         vBox.setAlignment(Pos.CENTER);
 
+        /**
+         * JavaFX view final setup occurs here
+         */
         Scene scene = new Scene (vBox);
         stage.setTitle("Filesort-GUI - " + VERSION);
         stage.setScene(scene);
